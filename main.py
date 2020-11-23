a0073e30b76bf5ff096bb332f2de863da180efa0
kanary <kanary159357@gmail.com>
2020-11-23 17:48:19 +0900

    page ListDownload implement

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from selenium import webdriver
-import  requests
+import requests
 import shutil
-def imgCrawler(URL):
-    driver = webdriver.Chrome('Chrome/chromedriver.exe')
-    driver.get(URL)
+import os
+import time
+import copy
+driver = webdriver.Chrome('Chrome/chromedriver.exe')
 
+
+def createFolder(directory):
+    os.makedirs(directory, exist_ok=True)
+
+
+def ListCrawler(URL):
+    driver.get(URL)
+    comicName = driver.find_element_by_class_name('text-left').text
+    comicList =driver.find_elements_by_class_name('list-subject>a')
+    comicArray = []
+    for item in comicList:
+        comicArray.append([item.get_attribute('href'), item.text])
+        print(comicArray[-1])
+
+    print(len(comicList))
+    createFolder('./' + comicName)
+    for item in comicArray:
+        comic_dir = './' + comicName + './' + item[1]
+        createFolder(comic_dir)
+        imgCrawler(item[0], comic_dir)
+
+
+
+def imgCrawler(URL, directory):
+    driver.get(URL)
     elements = driver.find_elements_by_css_selector('div.view-img>img')
     cnt = 0
     print(len(elements))
@@ -12,13 +38,14 @@ def imgCrawler(URL):
     for element in elements:
         imgUrl = element.get_attribute('src')
         print(imgUrl)
-        r = requests.get(imgUrl,stream = True)
+        r = requests.get(imgUrl, stream=True)
         if r.status_code == 200:
-            with open("img"+str(cnt)+".png", 'wb') as f:
+            with open(directory + '/' + str(cnt) + ".jpg", 'wb') as f:
                 r.raw.decode_content = True
-                shutil.copyfileobj(r.raw,f)
-        cnt+=1
+                shutil.copyfileobj(r.raw, f)
+        cnt += 1
+
 
 if __name__ == "__main__":
-    URL = 'https://marumaru.cash/bbs/cmoic/20033/145646'
-    imgCrawler(URL)
+    URL = 'https://marumaru.cash/bbs/cmoic/19760'
+    ListCrawler(URL)
